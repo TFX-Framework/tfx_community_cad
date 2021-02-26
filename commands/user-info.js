02759d9c5c4400607c9c5a17b33934d8e70cd7c6
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-02-26 15:23:17 -0700

    Update user-info.js

--- commands/user-info.js
+++ commands/user-info.js
@@ -11,7 +11,7 @@ module.exports.run = async (client , message, args) => {
 
     if (!user) return message.reply('Please provide a users username from the CAD')
      
-    let fetched_user = await Users.findOne({ user.username: the_user });
+    let fetched_user = await Users.user.findOne({ username: the_user });
 
    // if (!fetched_user) await new Users({ username: user }).save();
 
