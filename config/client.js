cebe3f94ac71b848dc68f6e3bf17b8205f950ebb
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-02-26 08:03:01 -0700

    Update client.js

--- config/client.js
+++ config/client.js
@@ -1,3 +1,7 @@
 exports.token = process.env.DISCORD_TOKEN
 exports.logo = 'https://toxicfx.org/images/TFX-Transparent.png'
 exports.color = '#00B0E1'
+exports.devs=[
+"510065483693817867", // Toxic Dev
+"486659270876856362" // Toxics Alt
+]
