bfcd94ea6f3de60a1b0153ec161fa3aef8f7aa72
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-02-26 15:37:19 -0700

    Update user-info.js

--- commands/user-info.js
+++ commands/user-info.js
@@ -11,7 +11,7 @@ module.exports.run = async (client , message, args) => {
 
     if (!the_user) return message.reply('Please provide a users username from the CAD')
      
-    let fetched_user = await Users.findOne({ username: the_user });
+    let fetched_user = await Users.findOne({ user: { username: the_user  }});
 
    // if (!fetched_user) await new Users({ user: user }).save();
 
@@ -19,8 +19,8 @@ module.exports.run = async (client , message, args) => {
     
     let embed = new MessageEmbed()
       .setAuthor("User Information", client.config.logo)
-      .setDescription(`${fetched_user.username}s Information`)
-      .addField("Name", `${fetched_user.name}`)
+      .setDescription(`${fetched_user.user.username}s Information`)
+      .addField("Name", `${fetched_user..user.name}`)
       .addField("Call Sign", `${fetched_user.user.callSign}`)
       .setFooter('© 2021 ToxicFX Community CAD', client.config.logo)
 
